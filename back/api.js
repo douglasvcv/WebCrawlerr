ae7b8beab808684b13cec9ea937642622357e895
Douglas <douglasvitor1233@gmail.com>
2024-02-27 20:37:40 -0300

    Adding api to get and insert data into mongoDB

--- back/api.js
+++ back/api.js
@@ -1,13 +1,65 @@
 import 'dotenv/config'
 
-export const search = async (city) => {
+import axios from 'axios'
+import { MongoClient } from 'mongodb'
+
+
+
+async function getWeatherForecast(city){
     try {
-        const url = `http://api.weatherapi.com/v1/forecast.json?key=`
-        const response = await fetch(url+process.env.APIKEY+`&q=${city}&days=1&aqi=no&alerts=no`)
-        const data = await response.json()
-        console.log(data)
-        return data
+        const apiKey = process.env.APIKEY
+        const url = "http://api.weatherapi.com/v1/forecast.json"
+        const response = await axios.get(url, {
+            params:{
+                key:apiKey,
+                q:city,
+                days:1
+            }
+        })
+        console.log(apiKey)
+        return response.data
     } catch (error) {
-        console.log(`Error: ${error}`)
+        console.error('Erro ao obter a previsão do tempo:', error);
+        return null;
     }
 }
+
+async function insertWeatherData(data){
+    const uri = process.env.DB_URI
+    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true })
+
+    try {
+        await client.connect()
+        const database = client.db('webcrawler')
+        const collection = database.collection('weather')
+
+        const result = await collection.insertOne(data)
+        console.log(`${result.insertedCount} documentos inseridos.`)
+    } catch (error) {
+        console.error('Erro ao inserir os dados no MongoDB Atlas:', error)
+    }finally{
+        await client.close()
+    }
+}
+
+const cities = ['brasilia']
+
+
+async function getAndInsertWeatherData(cities){
+    for(let city = 0; city < cities.length; city++){
+        const forecastData = await getWeatherForecast(cities[city])
+
+        if(forecastData){
+            console.log(`Previsão do tempo para ${cities[city]}:`);
+            console.log(forecastData);
+            console.log();
+
+            await insertWeatherData(forecastData)
+        }else{
+            console.log(`Não foi possível obter a previsão do tempo para ${cities[city]}`);
+        
+        }
+    }
+}
+
+getAndInsertWeatherData(cities)
